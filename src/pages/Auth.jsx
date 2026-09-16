13aee0727064487a11980132641c6b2ee1b92487
Paulo Avila <46868029+systemguy@users.noreply.github.com>
2026-09-16 16:37:31 -0500

    Update Auth.jsx to be integrated with backend

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import { useNavigate } from 'react-router-dom';
 // change this if your backend runs somewhere else
 const API_URL = 'http://localhost:3000';
 
@@ -9,27 +9,69 @@ function Auth() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
+  const navigate = useNavigate();
 
-  function handleSubmit(e) {
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+
+  //   const url = isLogin ? `${API_URL}/login` : `${API_URL}/register`;
+  //   const body = isLogin ? { email, password } : { name, email, password };
+
+  //   fetch(url, {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify(body),
+  //   })
+      
+  //     .then((res) => res.json(), console.log(body))
+  //     .then(() => {
+  //       setMessage(isLogin ? 'Logged in!' : 'Account created!');
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //       setMessage('Could not connect to the server yet.' +err);
+        
+  //     });
+  // }
+
+  const handleSubmit = async(e) =>{
     e.preventDefault();
+    
 
-    const url = isLogin ? `${API_URL}/login` : `${API_URL}/register`;
-    const body = isLogin ? { email, password } : { name, email, password };
+    try{
 
-    fetch(url, {
+      const url = isLogin ? `${API_URL}/login` : `${API_URL}/register`;
+      const body = isLogin ? { email, password } : { name, email, password };
+      const res = await fetch(url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body),
-    })
-      .then((res) => res.json())
-      .then(() => {
-        setMessage(isLogin ? 'Logged in!' : 'Account created!');
       })
-      .catch(() => {
-        setMessage('Could not connect to the server yet.');
-      });
+      
+      const data = await res.json()
+      console.log(data)
+      if(!res.ok){
+        console.log(data)
+        if (Array.isArray(data.error)) {
+          setMessage(data.error.join(', '));
+        }
+        else if (typeof data.error === 'string') {
+          setMessage(data.error);
+        }else{
+          setMessage(data.message || 'Invalid username or password')
+        }
+        return;
+      }
+      if (data.token) {
+        localStorage.setItem('token', data.token); // Save Bearer token
+      }
+      setMessage("Login successful")
+      navigate('/incidents')
+    }catch(err){
+      console.log(err)
+      setMessage("Could not connect to the server")
+    }
   }
-
   return (
     <div className="page">
       <h1>{isLogin ? 'Login' : 'Register'}</h1>
